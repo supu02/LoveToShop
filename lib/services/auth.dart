40f49268f537b80164f7f990de1799dc972fbef2
Supriya Malla <supriya.malla02@gmail.com>
2020-10-31 10:51:06 +0545

    Custom user model

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:love_to_shop/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
     }

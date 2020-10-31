e3fb7c942d343661938822c3585075b3eb023e8e
Supriya Malla <supriya.malla02@gmail.com>
2020-10-31 19:33:00 +0545

    Register and Sign in with Email and Password with Validation

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -29,6 +29,34 @@ class AuthService {
     }
   }
 
+  // Register with email and password
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign in with email and password
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   Future signOut() async {
     try {
       return await _auth.signOut();

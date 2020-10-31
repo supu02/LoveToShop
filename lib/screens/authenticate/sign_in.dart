0f49035607f44be226de3f858f874ff9629bc090
Supriya Malla <supriya.malla02@gmail.com>
2020-10-31 10:30:10 +0545

    Sign in anonymously and sign in UI

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:love_to_shop/shared/constants.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Love To Shop'),
+        backgroundColor: Color(0xff61c0bf),
+      ),
+      body: Container(
+        color: Color(0xfffae3d9),
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(padding: EdgeInsets.only(top: 80.0)),
+            CircleAvatar(
+              child: Image.asset('assets/cart.png'),
+              radius: 80.0,
+              backgroundColor: Colors.transparent,
+            ),
+            Form(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: kTextInputDecoration.copyWith(
+                        hintText: 'Enter your email'),
+                  ),
+                  SizedBox(
+                    height: 13.0,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: kTextInputDecoration.copyWith(
+                        hintText: 'Enter your password'),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  RaisedButton(
+                    color: Color(0xffffb6b9),
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(color: Colors.white, fontSize: 17.0),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kTextInputDecoration = InputDecoration(
+    fillColor: Colors.white,
+    filled: true,
+    hintText: 'Enter a value',
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(32.0),
+      ),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xffbbded6),
+        width: 1.0,
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0xffbbded6), width: 2.0),
+        borderRadius: BorderRadius.all(Radius.circular(32.0))));
